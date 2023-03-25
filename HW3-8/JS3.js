bb69f6eed2efbb19465894f0788350ea3b2e9670
tsukanovannn <tsukanovannn@gmail.com>
2023-03-25 21:44:37 +0200

    HW3-8

---
+++ HW3-8/JS3.js
@@ -0,0 +1,9 @@
+let number1 = prompt('Write number 1');
+let number2 = prompt('Write number 1');
+num1 = Number(number1);
+num2 = Number(number2);
+
+alert(num1 + num2);
+alert(num1 - num2);
+alert(num1 * num2);
+alert(num1 / num2);
--- HW3/JS3.js
+++
@@ -1,2 +0,0 @@
-const a = false
-console.log(!a)
