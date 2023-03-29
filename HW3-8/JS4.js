350a27dd476503dfbb9d713643d2bfbd3a209d97
tsukanovannn <tsukanovannn@gmail.com>
2023-03-29 19:32:22 +0300

    HW3-8

--- HW3-8/JS4.js
+++ HW3-8/JS4.js
@@ -1,34 +1,29 @@
 let str = prompt('Select actions!')
 let number1 = prompt('Write number 1');
-let number2 = prompt('Write number 1');
+let number2 = prompt('Write number 2');
 num1 = Number(number1);
 num2 = Number(number2);
 
-// if (str === 'add'){
-//     alert(num1 + num2)
-// }   else if (str === 'sub'){
-//     alert(num1 - num2)
-// }   else if (str === 'mult'){
-//     alert(num1 * num2)
-// }   else if (str === 'div'){
-//     alert(num1 / num2)
-// }   else{
-//         alert('error')
-// }
+if (isNaN(num1) || isNaN(num2)){
+    alert("The value enter is not a number, please try again.")
+}
 
-switch (str) {
-    case 'add':
-        alert(num1 + num2)
-        break
-    case 'sub':
-        alert(num1 - num2)
-        break
-    case 'mult':
-        alert(num1 * num2)
-        break
-    case 'div':
-        alert(num1 / num2)
-        break
-    default:
-        alert(`error`)
+if (str !== "add" && str !== "sub" && str !== "mult" && str !== "div") {
+    alert("Incorrect operation entered. Please enter add, sub, mult or div.")
+}
+
+if (num2 === 0 && str === "div"){
+    alert("Division by 0 is impossible. Please enter a different number")
+}
+
+if (str === 'add'){
+    alert(num1 + num2)
+}   else if (str === 'sub'){
+    alert(num1 - num2)
+}   else if (str === 'mult'){
+    alert(num1 * num2)
+}   else if (str === 'div'){
+    alert(num1 / num2)
+}   else{
+        alert('error')
 }
