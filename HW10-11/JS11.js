de2ea8a50e4df8500f788c072d8d8db21e3e61e9
tsukanovannn <tsukanovannn@gmail.com>
2023-04-03 23:09:35 +0300

    HW10-11

---
+++ HW10-11/JS11.js
@@ -0,0 +1,13 @@
+// let message = "";
+// for (let i = 20; i < 30; i++) {
+//     let result = i + 0.5;
+//     message += result + "\n";
+// }
+// alert(message)
+
+// let message = "";
+// for (let i = 10; i <= 100; i = i +10) {
+//     let result = i * 27
+//     message += result + "\n"
+// }
+// alert(message)
